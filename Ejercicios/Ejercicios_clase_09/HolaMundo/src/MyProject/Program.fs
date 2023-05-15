5038d58c547de66c1ad93ddbb35fb74d8d3df48f
Pablo Chehade <pablo.chehade.villalba@gmail.com>
2023-05-15 17:30:06 -0300

    Fui haciendo cambios a las librerías y proyectos creados en la clase 9

--- Ejercicios/Ejercicios_clase_09/HolaMundo/src/MyProject/Program.fs
+++ Ejercicios/Ejercicios_clase_09/HolaMundo/src/MyProject/Program.fs
@@ -1,2 +1,26 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+printfn "Hello from F# v2"
+
+
+//Importo la biblioteca MyLibrary
+open MyLibrary
+
+
+[<EntryPoint>]
+let main args =
+    //Imprimir cada elemento del array de strings args
+    for arg in args do
+        MyLibrary.Say.hello arg
+    // printfn "Arguments passed to function : %A" args
+    // Return 0. This indicates success.
+    0
+
+//Para ejecutar el código en la consola, escribir en la terminal:
+//dotnet run
+//Para compilar el código, escribir en la terminal:
+//dotnet build
+//Para crear un ejecutable, escribir en la terminal:
+//dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true
+
+
+
